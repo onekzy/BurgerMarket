1d16269c68ceca430e270741d0de83292cf4d3f9
onekzy <hamert4@yandex.ru>
2020-06-13 10:06:48 +0300

    init commit

---
+++ src/main/java/com/market/burgermarket/BurgermarketApplication.java
@@ -0,0 +1,13 @@
+package com.market.burgermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurgermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BurgermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/market/burgermarket/BurgermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.market.burgermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurgermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
